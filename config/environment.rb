a53c3b47890fa8b1a1b26767485d34a86b36d27c
Minchul An <min@minchulan.io>
2023-02-05 17:27:54 -0700

    commit tree 70ab4cefe54ca838625757e619cb6b5cd44e5f91

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,11 @@
+# In this file, we're requiring in the environment.rb file (which loads the code for our database connection, as well as the Song class), and has a binding.pry to set a breakpoint where you can enter a Pry session.
+
 require 'bundler'
 Bundler.require
 
 require_relative '../lib/song'
 
+# constant, DB, that is equal to a hash that contains our connection to the database 
 DB = { conn: SQLite3::Database.new("db/music.db") }
+
+
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,24 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id 
 
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id 
     @name = name
     @album = album
   end
+
+  # To "map" our class to a database table, we will create a table with the same name, pluralized, as our class, and give that table column names that match the attr_accessors of our class. Will map instance attributes to table columns.
+
+  def self.create_table # class method creates this DB table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs ( 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql) # access the DB constant and database connection like this DB[:conn]. Call execute and pass in sql.
+  end 
 
 end
